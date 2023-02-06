dfbc6fef048887a28fca7d8b4ccfe30b9434b9a1
Atanas Atanasov <hi@atanas.info>
2023-02-06 16:24:02 +0200

    Use persistent cookie header

--- api/analytics.ts
+++ api/analytics.ts
@@ -4,12 +4,14 @@ import { join } from 'path';
 import { randomBytes } from 'crypto';
 import { createReadStream } from 'fs';
 
+const COOKIE_HEADER = 'x-ga-beacon-id';
+
 export default async function handler(req: VercelRequest, res: VercelResponse): Promise<void> {
 	const {
 		query: { flat, page, pixel, account }
 	} = req;
 
-	const cid = randomBytes(16).toString('hex');
+	const cid = req.headers[COOKIE_HEADER] || randomBytes(16).toString('hex');
 	const url = `http://www.google-analytics.com/collect?v=1&t=pageview&cid=${cid}&tid=${account}&dp=${page}`;
 
 	const isFlat = typeof flat !== 'undefined';
@@ -40,6 +42,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse): 
 
 	await new Promise(resolve => {
 		res.setHeader('Content-Type', 'image/svg+xml');
+		res.setHeader(COOKIE_HEADER, cid);
 
 		imageStream.pipe(res);
 		imageStream.on('end', resolve);
