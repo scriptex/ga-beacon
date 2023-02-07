447416d95d924ba82e32a54b01a117fcbba16fda
Atanas Atanasov <hi@atanas.info>
2023-02-07 11:15:47 +0200

    Update frontend

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,41 @@
 // @ts-nocheck
 import htm from 'https://cdn.skypack.dev/htm';
 import { h, render } from 'https://cdn.skypack.dev/preact';
-import { useMemo, useState, useCallback } from 'https://cdn.skypack.dev/preact/hooks';
+import { useRef, useMemo, useState, useEffect, useCallback } from 'https://cdn.skypack.dev/preact/hooks';
 
 const html = htm.bind(h);
 
 function Result({ id, page, type }) {
+	const timeout = useRef(null);
+	const [copied, setCopied] = useState(false);
+
+	useEffect(() => {
+		return () => {
+			clearTimeout(timeout.current);
+		};
+	}, []);
+
 	const data = useMemo(
 		() => `https://ga-beacon.atanas.info/api/analytics?account=${id}&page=${page}${type}`,
 		[id, page, type]
 	);
+
+	const onCopy = useCallback(async () => {
+		await navigator.clipboard.writeText(data);
+
+		setCopied(true);
+
+		timeout.current = setTimeout(() => {
+			setCopied(false);
+		}, 1000);
+	});
 
 	if (id && page) {
 		return html`
 			<section>
 				<p>${data}</p>
 
-				<button onClick=${async () => await navigator.clipboard.writeText(data)}>copy</button>
+				<button onClick=${onCopy}>copy <span class=${copied ? 'visible' : undefined}>Copied!</span></button>
 			</section>
 		`;
 	}
@@ -27,8 +46,8 @@ function Result({ id, page, type }) {
 function App() {
 	const types = ['default', 'flat', 'pixel'];
 
-	const [id, setId] = useState('test');
-	const [page, setPage] = useState('test');
+	const [id, setId] = useState('');
+	const [page, setPage] = useState('');
 	const [image, setImage] = useState(types[0]);
 
 	const onSetId = useCallback(e => setId(e.target.value), [id, setId]);
@@ -41,13 +60,19 @@ function App() {
 		<div>
 			<label for="id">Google Analytics ID</label>
 
-			<input type="text" id="id" value="${id}" onInput="${onSetId}" />
+			<input id="id" type="text" value="${id}" onInput="${onSetId}" placeholder="UA-83446952-1" />
 		</div>
 
 		<div>
 			<label for="page">Page to be captured</label>
 
-			<input type="text" id="page" value="${page}" onInput="${onSetPage}" />
+			<input
+				id="page"
+				type="text"
+				value="${page}"
+				onInput="${onSetPage}"
+				placeholder="github.com/scriptex/ga-beacon"
+			/>
 		</div>
 
 		<div>
